0715139bb05d2fa6396c684cd569759cceff2134
yj77iw0 <treeyu12345@naver.com>
2023-07-04 13:59:23 +0900

    feat :: TextField 위젯 커스텀하기

---
+++ yoochanhong_and_children/lib/component/text_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    Key? key,
+    required this.width,
+    required this.controller,
+    required this.fontSize,
+    required this.maxLength,
+    required this.autofocus,
+    this.height,
+    this.hintText,
+    this.helperText,
+    this.textAlign,
+    this.textInputType,
+  }) : super(key: key);
+
+  final double width;
+  final double? height;
+
+  final int? maxLength;
+  final int fontSize;
+  final TextEditingController controller;
+  final String? hintText, helperText;
+  final TextInputType? textInputType;
+  final TextAlign? textAlign;
+  final bool autofocus;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width.w,
+      height: widget.height != null ? widget.height!.h : null,
+      child: TextFormField(
+        controller: widget.controller,
+        autofocus: widget.autofocus,
+        keyboardType: widget.textInputType,
+        textAlign: widget.textAlign != null ? widget.textAlign! : TextAlign.start,
+        onChanged: (value) => setState(() {}),
+        maxLength: widget.maxLength,
+      ),
+    );
+  }
+}
