c8f49116530225e23e092e59b44fa00b3aef08f0
yoochanhong <chanhongy6@naver.com>
2023-07-05 09:59:58 +0900

    feat :: 채팅하러 가기 버튼 퍼블리싱

--- yoochanhong_and_children/lib/screen/main_page.dart
+++ yoochanhong_and_children/lib/screen/main_page.dart
@@ -70,6 +70,38 @@ class MainPage extends StatelessWidget {
                 color: const Color(0xffD7EBD7),
                 borderRadius: BorderRadius.circular(20.0),
               ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      "assets/images/chat.png",
+                      width: 60,
+                      height: 55,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: 190,
+                        height: 62,
+                        decoration: BoxDecoration(
+                          color: const Color(0xff6BB86A),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "채팅하러 가기",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
