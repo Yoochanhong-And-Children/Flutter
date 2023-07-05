e01e79b66acb893c691216aab97b2db5f8858409
yoochanhong <chanhongy6@naver.com>
2023-07-05 14:07:19 +0900

    feat :: 메시지 컨테이너 생성

--- yoochanhong_and_children/lib/screen/chatting_page.dart
+++ yoochanhong_and_children/lib/screen/chatting_page.dart
@@ -10,7 +10,7 @@ class ChattingPage extends StatefulWidget {
 }
 
 class _ChattingPageState extends State<ChattingPage> {
-  bool _isTyping = false;
+  bool _isTyping = false, itMe = false;
 
   late TextEditingController textEditingController;
   late ScrollController scrollController;
@@ -51,6 +51,65 @@ class _ChattingPageState extends State<ChattingPage> {
           style: TextStyle(fontSize: 28.0.sp, color: Colors.black),
         ),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 7,
+            child: ScrollConfiguration(
+              behavior: const ScrollBehavior().copyWith(overscroll: false),
+              child: Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                width: MediaQuery.of(context).size.width - 40,
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Directionality(
+                          textDirection:
+                              itMe ? TextDirection.ltr : TextDirection.rtl,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 279.0.w,
+                                height: 59.0.h,
+                                decoration: const BoxDecoration(
+                                  color: Color(0xff6EB96C),
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(25.0),
+                                    topRight: Radius.circular(25.0),
+                                    bottomLeft: Radius.circular(25.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                ),
+                Container(),
+                Container(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
