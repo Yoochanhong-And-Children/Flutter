21be3215daf6aa741399d0070f1cf3286c506362
yj77iw0 <treeyu12345@naver.com>
2023-07-05 12:10:11 +0900

    feat :: 텍스트 필드에 포커스 추가

--- yoochanhong_and_children/lib/screen/my_page.dart
+++ yoochanhong_and_children/lib/screen/my_page.dart
@@ -14,6 +14,9 @@ class _MyPageState extends State<MyPage> {
   TextEditingController protectorNumber1Controller = TextEditingController();
   TextEditingController protectorNumber2Controller = TextEditingController();
   TextEditingController protectorNumber3Controller = TextEditingController();
+  FocusNode protectorNum1FocusNode = FocusNode();
+  FocusNode protectorNum2FocusNode = FocusNode();
+  FocusNode protectorNum3FocusNode = FocusNode();
 
   @override
   void initState() {
@@ -47,89 +50,70 @@ class _MyPageState extends State<MyPage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 30.0.w),
-                child: Text(
-                  '유하은 님',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 32.sp,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 40.0.h),
-          Container(
-            width: 346.0.w,
-            height: 44.0.h,
-            decoration: BoxDecoration(
-              color: Color(0xFF5A9D59),
-              borderRadius: BorderRadius.circular(50.r),
-            ),
-            child: Center(
-              child: Text(
-                '노인을 위한 나라는 없다',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.sp,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 196.0.h),
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 30.0.w),
-                child: Text(
-                  '보호자 정보',
-                  style: TextStyle(
-                    color: Color(0xFF5A9D59),
-                    fontSize: 22.sp
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10.0.h),
-          Container(
-            width: 370.0.w,
-            height: 90.0.h,
-            decoration: BoxDecoration(
-              color: Color(0xFF5A9D59),
-              borderRadius: BorderRadius.circular(20.r),
-            ),
-            child: Row(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
               children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: SizedBox(
-                    width: 140.0.w,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.0.w),
-                      child: TextFormField(
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.sp,
-                        ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none
-                        ),
-                        controller: protectorNameController,
-                      ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0.w),
+                  child: Text(
+                    '유하은 님',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32.sp,
                     ),
                   ),
                 ),
-                SizedBox(width: 47.0.w),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 56.0.w,
+              ],
+            ),
+            SizedBox(height: 40.0.h),
+            Container(
+              width: 346.0.w,
+              height: 44.0.h,
+              decoration: BoxDecoration(
+                color: Color(0xFF5A9D59),
+                borderRadius: BorderRadius.circular(50.r),
+              ),
+              child: Center(
+                child: Text(
+                  '노인을 위한 나라는 없다',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 196.0.h),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0.w),
+                  child: Text(
+                    '보호자 정보',
+                    style: TextStyle(
+                      color: Color(0xFF5A9D59),
+                      fontSize: 22.sp
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0.h),
+            Container(
+              width: 370.0.w,
+              height: 90.0.h,
+              decoration: BoxDecoration(
+                color: Color(0xFF5A9D59),
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: Row(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: SizedBox(
+                      width: 140.0.w,
                       child: Padding(
                         padding: EdgeInsets.only(left: 20.0.w),
                         child: TextFormField(
@@ -138,80 +122,121 @@ class _MyPageState extends State<MyPage> {
                             fontSize: 20.sp,
                           ),
                           decoration: InputDecoration(
-                              border: InputBorder.none
+                            border: InputBorder.none
                           ),
-                          controller: protectorNumber1Controller,
+                          controller: protectorNameController,
                         ),
                       ),
                     ),
-                    Text(
-                      '-',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.sp,
+                  ),
+                  SizedBox(width: 47.0.w),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 56.0.w,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 20.0.w),
+                          child: TextFormField(
+                            maxLength: 3,
+                            onChanged: (value) {
+                              if (value.length == 3) {
+                                FocusScope.of(context).requestFocus(protectorNum2FocusNode);
+                              }
+                            },
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.sp,
+                            ),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              counterText: '',
+                            ),
+                            focusNode: protectorNum1FocusNode,
+                            controller: protectorNumber1Controller,
+                          ),
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 47.0.w,
-                      child: TextFormField(
-                        maxLength: 4,
+                      Text(
+                        '-',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20.sp,
                         ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          counterText: '',
+                      ),
+                      SizedBox(
+                        width: 47.0.w,
+                        child: TextFormField(
+                          maxLength: 4,
+                          onChanged: (value) {
+                            if (value.length == 4) {
+                              FocusScope.of(context).requestFocus(protectorNum3FocusNode);
+                            }
+                          },
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.sp,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            counterText: '',
+                          ),
+                          focusNode: protectorNum2FocusNode,
+                          controller: protectorNumber2Controller,
                         ),
-                        controller: protectorNumber2Controller,
                       ),
-                    ),
-                    Text(
-                      '-',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.sp,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 47.0.w,
-                      child: TextFormField(
-                        maxLength: 4,
+                      Text(
+                        '-',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20.sp,
                         ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          counterText: '',
-                        ),
-                        controller: protectorNumber3Controller,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 47.0.w,
+                        child: TextFormField(
+                          maxLength: 4,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.sp,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            counterText: '',
+                          ),
+                          focusNode: protectorNum3FocusNode,
+                          controller: protectorNumber3Controller,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15.0.h),
+            GestureDetector(
+              onTap: () {
+
+              },
+              child: Container(
+                width: 74.0.w,
+                height: 40.0.h,
+                decoration: BoxDecoration(
+                  color: Color(0xFFDAEEDA),
+                  borderRadius: BorderRadius.circular(15.r)
                 ),
-              ],
-            ),
-          ),
-          SizedBox(height: 15.0.h),
-          Container(
-            width: 74.0.w,
-            height: 40.0.h,
-            decoration: BoxDecoration(
-              color: Color(0xFFDAEEDA),
-              borderRadius: BorderRadius.circular(15.r)
-            ),
-            child: Center(
-              child: Text(
-                '변경',
-                style: TextStyle(
-                  color: Color(0xFF5A9D59),
-                  fontSize: 18.sp,
+                child: Center(
+                  child: Text(
+                    '변경',
+                    style: TextStyle(
+                      color: Color(0xFF5A9D59),
+                      fontSize: 18.sp,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
