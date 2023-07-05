b990585d4369f79271086c973eddd2d382a7ef07
yj77iw0 <treeyu12345@naver.com>
2023-07-05 21:38:08 +0900

    feat :: 알림 시간 설정 기능 추가

--- yoochanhong_and_children/lib/screen/my_page.dart
+++ yoochanhong_and_children/lib/screen/my_page.dart
@@ -19,7 +19,45 @@ class _MyPageState extends State<MyPage> {
   FocusNode protectorNum2FocusNode = FocusNode();
   FocusNode protectorNum3FocusNode = FocusNode();
 
+  late FixedExtentScrollController riceTimeScrollController =
+  FixedExtentScrollController();
+
+  List<String> riceType = [
+    '아침',
+    '점심',
+    '저녁',
+  ];
+
+  List<String> riceTime = [
+    '0시',
+    '1시',
+    '2시',
+    '3시',
+    '4시',
+    '5시',
+    '6시',
+    '7시',
+    '8시',
+    '9시',
+    '10시',
+    '11시',
+    '12시',
+    '13시',
+    '14시',
+    '15시',
+    '16시',
+    '17시',
+    '18시',
+    '19시',
+    '20시',
+    '21시',
+    '22시',
+    '23시',
+  ];
+
   bool alarmSetting = false;
+
+  int selectedPickerIndex = 0;
 
   @override
   void initState() {
@@ -28,6 +66,7 @@ class _MyPageState extends State<MyPage> {
     protectorNumber1Controller.text = '010';
     protectorNumber2Controller.text = '1234';
     protectorNumber3Controller.text = '5678';
+    riceTimeScrollController = FixedExtentScrollController(initialItem: 0);
   }
 
   @override
@@ -37,6 +76,17 @@ class _MyPageState extends State<MyPage> {
     protectorNumber1Controller.dispose();
     protectorNumber2Controller.dispose();
     protectorNumber3Controller.dispose();
+  }
+
+  void nextPicker() {
+    setState(() {
+      if (selectedPickerIndex == 2) {
+        Navigator.of(context).pop();
+        selectedPickerIndex = 0;
+      } else if (selectedPickerIndex < 2) {
+        selectedPickerIndex++;
+      }
+    });
   }
 
   @override
@@ -52,7 +102,7 @@ class _MyPageState extends State<MyPage> {
             onPressed: () {
               Navigator.of(context).pop();
             },
-            icon: Icon(
+            icon: const Icon(
               Icons.arrow_back_ios_new_rounded,
               color: Colors.black,
             ),
@@ -95,7 +145,7 @@ class _MyPageState extends State<MyPage> {
               width: 346.0.w,
               height: 44.0.h,
               decoration: BoxDecoration(
-                color: Color(0xFF5A9D59),
+                color: const Color(0xFF5A9D59),
                 borderRadius: BorderRadius.circular(50.r),
               ),
               child: Center(
@@ -109,39 +159,127 @@ class _MyPageState extends State<MyPage> {
                 ),
               ),
             ),
-            SizedBox(height: 60.0.h),
+            SizedBox(height: 75.0.h),
             Align(
               alignment: Alignment.centerLeft,
               child: Padding(
                 padding: EdgeInsets.only(left: 30.0.w),
-                child: SizedBox(
-                  width: 250.0.w,
-                  height: 44.0.h,
-                  child: Row(
-                    children: [
-                      Text(
-                        '식사 시간 알림',
-                        style: TextStyle(
-                          color: Color(0xFF5A9D59),
-                          fontSize: 22.sp,
-                          fontFamily: 'ExtraBold',
+                child: GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return StatefulBuilder(
+                          builder: (context, setState) {
+                            return CupertinoAlertDialog(
+                              title: Text(
+                                riceType[selectedPickerIndex],
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.sp,
+                                  fontFamily: 'ExtraBold',
+                                ),
+                              ),
+                              content: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 20.0.h,
+                                  ),
+                                  CupertinoPicker(
+                                    scrollController: riceTimeScrollController,
+                                    itemExtent: 50.0.h,
+                                    onSelectedItemChanged: (int index) {
+                                      setState(() {
+
+                                      });
+                                    },
+                                    children: riceTime
+                                        .map<Widget>((item) => Center(
+                                          child: Text(
+                                      item,
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 24.sp,
+                                          fontFamily: 'ExtraBold',
+                                      ),
+                                    ),
+                                        ))
+                                        .toList(),
+                                  ),
+                                  const SizedBox(height: 16.0),
+                                ],
+                              ),
+                              actions: [
+                                CupertinoDialogAction(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                    selectedPickerIndex = 0;
+                                  },
+                                  child: Text(
+                                    '취소',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18.sp,
+                                      fontFamily: 'ExtraBold',
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  color: const Color(0xFF5A9D59),
+                                  child: CupertinoDialogAction(
+                                    onPressed: () {
+                                      setState(() {
+                                        nextPicker();
+                                      });
+                                    },
+                                    child: Text(
+                                      '다음',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.sp,
+                                        fontFamily: 'ExtraBold',
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    );
+
+                  },
+                  child: Container(
+                    width: 370.0.w,
+                    height: 90.0.h,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF5A9D59),
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Row(
+                      children: [
+                        SizedBox(width: 50.0.w),
+                        Text(
+                          '식사 시간 알림 설정하기',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.sp,
+                            fontFamily: 'ExtraBold',
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 30.0.w),
-                      CupertinoSwitch(
-                        value: alarmSetting,
-                        onChanged: (bool value) {
-                          setState(() {
-                            alarmSetting = value;
-                          });
-                        },
-                      ),
-                    ],
+                        SizedBox(width: 40.0.w),
+                        const Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 86.0.h),
+            SizedBox(height: 30.0.h),
             Row(
               children: [
                 Padding(
@@ -149,7 +287,7 @@ class _MyPageState extends State<MyPage> {
                   child: Text(
                     '보호자 정보',
                     style: TextStyle(
-                      color: Color(0xFF5A9D59),
+                      color: const Color(0xFF5A9D59),
                       fontSize: 22.sp,
                       fontFamily: 'ExtraBold',
                     ),
@@ -162,7 +300,7 @@ class _MyPageState extends State<MyPage> {
               width: 370.0.w,
               height: 90.0.h,
               decoration: BoxDecoration(
-                color: Color(0xFF5A9D59),
+                color: const Color(0xFF5A9D59),
                 borderRadius: BorderRadius.circular(20.r),
               ),
               child: Row(
@@ -179,7 +317,7 @@ class _MyPageState extends State<MyPage> {
                             fontSize: 20.sp,
                             fontFamily: 'ExtraBold',
                           ),
-                          decoration: InputDecoration(
+                          decoration: const InputDecoration(
                             border: InputBorder.none
                           ),
                           controller: protectorNameController,
@@ -206,7 +344,7 @@ class _MyPageState extends State<MyPage> {
                               fontSize: 20.sp,
                               fontFamily: 'Heavy'
                             ),
-                            decoration: InputDecoration(
+                            decoration: const InputDecoration(
                               border: InputBorder.none,
                               counterText: '',
                             ),
@@ -237,7 +375,7 @@ class _MyPageState extends State<MyPage> {
                             fontSize: 20.sp,
                             fontFamily: 'Heavy',
                           ),
-                          decoration: InputDecoration(
+                          decoration: const InputDecoration(
                             border: InputBorder.none,
                             counterText: '',
                           ),
@@ -262,7 +400,7 @@ class _MyPageState extends State<MyPage> {
                             fontSize: 20.sp,
                             fontFamily: 'Heavy',
                           ),
-                          decoration: InputDecoration(
+                          decoration: const InputDecoration(
                             border: InputBorder.none,
                             counterText: '',
                           ),
@@ -318,7 +456,7 @@ class _MyPageState extends State<MyPage> {
                                   height: 55.0.h,
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(20.r),
-                                    color: Color(0xFFC3C3C3),
+                                    color: const Color(0xFFC3C3C3),
                                   ),
                                   child: Center(
                                     child: Text(
@@ -344,7 +482,7 @@ class _MyPageState extends State<MyPage> {
                                   height: 55.0.h,
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(20.r),
-                                    color: Color(0xFF3F853E),
+                                    color: const Color(0xFF3F853E),
                                   ),
                                   child: Center(
                                     child: Text(
@@ -370,14 +508,14 @@ class _MyPageState extends State<MyPage> {
                 width: 74.0.w,
                 height: 40.0.h,
                 decoration: BoxDecoration(
-                  color: Color(0xFFDAEEDA),
+                  color: const Color(0xFFDAEEDA),
                   borderRadius: BorderRadius.circular(15.r)
                 ),
                 child: Center(
                   child: Text(
                     '변경',
                     style: TextStyle(
-                      color: Color(0xFF5A9D59),
+                      color: const Color(0xFF5A9D59),
                       fontSize: 18.sp,
                       fontFamily: 'ExtraBold',
                     ),
