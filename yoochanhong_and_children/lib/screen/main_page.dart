3dfd17c02f6b373794460ca59e3620d355d4a382
yoochanhong <chanhongy6@naver.com>
2023-07-05 09:37:03 +0900

    feat :: 배경 색 추가

---
+++ yoochanhong_and_children/lib/screen/main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yoochanhong_and_children/common/common.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.backgroundColor,
+    );
+  }
+}
