f4a05a401ae3b7fb90a2a656f3646fadd06f5d19
yoochanhong <chanhongy6@naver.com>
2023-07-05 10:37:58 +0900

    feat :: 메인 페이지 퍼블리싱

--- yoochanhong_and_children/lib/screen/main_page.dart
+++ yoochanhong_and_children/lib/screen/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yoochanhong_and_children/common/common.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -16,145 +17,66 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       backgroundColor: CustomColor.backgroundColor,
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 110),
-            Text(
-              "유하은 님",
-              style: TextStyle(
-                fontSize: 40,
-              ),
-            ),
-            Text(
-              "식사는 하셨나요?",
-              style: TextStyle(
-                fontSize: 40,
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "보호자 정보",
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Color(0xff5A9D59),
-                  ),
-                ),
-                Container(
-                  width: 328,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: const Color(0xff5A9D59),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          "양채원",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                          ),
-                        ),
-                        Text(
-                          "010-5845-4856",
-                          style: TextStyle(color: Colors.white, fontSize: 24.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 100),
-            Container(
-              width: 320.0,
-              height: 260.0,
-              decoration: BoxDecoration(
-                color: const Color(0xffD7EBD7),
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child: PageView(
-                pageSnapping: true,
-                controller: pageController,
-                scrollDirection: Axis.horizontal,
+        child: Padding(
+          padding: EdgeInsets.only(left: 30.0.w, right: 30.0.w, top: 30.0.h),
+          child: Column(
+            children: [
+              SizedBox(height: 80.0.h),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 320.0,
-                    height: 260.0,
-                    decoration: BoxDecoration(
-                        color: const Color(0xffD7EBD7),
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image.asset(
-                            "assets/images/chat.png",
-                            width: 60,
-                            height: 55,
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: 190,
-                              height: 62,
-                              decoration: BoxDecoration(
-                                color: const Color(0xff6BB86A),
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "채팅하러 가기",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24.0,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "유하은 님",
+                        style: TextStyle(
+                          fontSize: 40.0.sp,
+                        ),
                       ),
+                      Text(
+                        "식사는 하셨나요?",
+                        style: TextStyle(
+                          fontSize: 40.0.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 40.0.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "보호자 정보",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Color(0xff5A9D59),
                     ),
                   ),
                   Container(
-                    width: 320.0,
-                    height: 260.0,
+                    width: 370.0.w,
+                    height: 90.0.h,
                     decoration: BoxDecoration(
-                        color: const Color(0xffD7EBD7),
-                        borderRadius: BorderRadius.circular(20.0)),
+                      color: const Color(0xff5A9D59),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
                     child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          Image.asset(
-                            "assets/images/person.png",
-                            width: 49,
-                            height: 63,
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: 190,
-                              height: 62,
-                              decoration: BoxDecoration(
-                                color: const Color(0xff6BB86A),
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "내 정보 보러가기",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24.0,
-                                  ),
-                                ),
-                              ),
+                          Text(
+                            "양채원",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28.0.sp,
                             ),
+                          ),
+                          Text(
+                            "010-5845-4856",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 28.0.sp),
                           ),
                         ],
                       ),
@@ -162,8 +84,102 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 100.0.h),
+              Container(
+                width: 320.0,
+                height: 260.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xffD7EBD7),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: PageView(
+                  pageSnapping: true,
+                  controller: pageController,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      width: 320.0,
+                      height: 260.0,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffD7EBD7),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                              "assets/images/chat.png",
+                              width: 60,
+                              height: 55,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 190,
+                                height: 62,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff6BB86A),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "채팅하러 가기",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24.0.sp,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 320.0,
+                      height: 260.0,
+                      decoration: BoxDecoration(
+                          color: const Color(0xffD7EBD7),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image.asset(
+                              "assets/images/person.png",
+                              width: 49,
+                              height: 63,
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: 190,
+                                height: 62,
+                                decoration: BoxDecoration(
+                                  color: const Color(0xff6BB86A),
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "내 정보 보러가기",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24.0.sp,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
